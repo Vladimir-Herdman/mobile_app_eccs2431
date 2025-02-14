fa4da6a1a8e4d61328963aaf5e4cfede262049b0
Vladimir Herdman <vovaherdman@gmail.com>
2025-02-14 13:29:49 -0500

    Vova, added full directory just in case, and little changes and readme

---
+++ pizza_price/PizzaPriceCalc/app/src/main/java/com/zybooks/pizzapricecalc/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.zybooks.pizzapricecalc
+
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val diameterInputA = findViewById<EditText>(R.id.diameterInputA)
+        val priceInputA = findViewById<EditText>(R.id.priceInputA)
+        val recInputA = findViewById<EditText>(R.id.recInputA)
+        val radioGroupA = findViewById<RadioGroup>(R.id.shapeRadioGroupA)
+
+        val diameterInputB = findViewById<EditText>(R.id.diameterInputB)
+        val priceInputB = findViewById<EditText>(R.id.priceInputB)
+        val recInputB = findViewById<EditText>(R.id.recInputB)
+        val radioGroupB = findViewById<RadioGroup>(R.id.shapeRadioGroupB)
+
+        val calculateButton = findViewById<Button>(R.id.calculateButton)
+        val resultTextA = findViewById<TextView>(R.id.resultTextA)
+        val resultTextB = findViewById<TextView>(R.id.resultTextB)
+        val finalChoice = findViewById<TextView>(R.id.finalChoice)
+
+        radioGroupA.setOnCheckedChangeListener {_, selectedShapeIdA, ->
+            when (selectedShapeIdA) {
+                R.id.radioCircleA -> recInputA.visibility = View.GONE
+                R.id.radioRectangleA -> recInputA.visibility = View.VISIBLE
+            }
+        }
+
+        radioGroupB.setOnCheckedChangeListener {_, selectedShapeIdB, ->
+            when (selectedShapeIdB) {
+                R.id.radioCircleB -> recInputB.visibility = View.GONE
+                R.id.radioRectangleB -> recInputB.visibility = View.VISIBLE
+            }
+        }
+
+        calculateButton.setOnClickListener {
+            val diameterA = diameterInputA.text.toString().toDoubleOrNull()
+            val widthA = recInputA.text.toString().toDoubleOrNull()
+            val priceA = priceInputA.text.toString().toDoubleOrNull()
+            val selectedShapeIdA = radioGroupA.checkedRadioButtonId
+            var sizeA = 1.0
+
+            var finalPriceA: Double = 1.0
+            var finalPriceB: Double = 1.0
+
+            if (diameterA != null && priceA != null && selectedShapeIdA != -1) {
+                val inchesSquaredA = when (selectedShapeIdA) {
+                    R.id.radioCircleA -> Math.PI * Math.pow((diameterA / 2), 2.0)
+                    R.id.radioRectangleA -> diameterA * (widthA ?: 1.0)
+                    else -> 0.0
+                }
+
+                finalPriceA = priceA * inchesSquaredA
+                sizeA = inchesSquaredA
+                resultTextA.text = "Final Price A: \$${String.format("%.2f", finalPriceA)}"
+            } else {
+                resultTextA.text = "Please enter valid inputs."
+            }
+
+            val diameterB = diameterInputB.text.toString().toDoubleOrNull()
+            val widthB = recInputB.text.toString().toDoubleOrNull()
+            val priceB = priceInputB.text.toString().toDoubleOrNull()
+            val selectedShapeIdB = radioGroupB.checkedRadioButtonId
+            var sizeB = 1.0
+
+            if (diameterB != null && priceB != null && selectedShapeIdB != -1) {
+                val inchesSquaredB = when (selectedShapeIdB) {
+                    R.id.radioCircleB -> Math.PI * Math.pow((diameterB / 2), 2.0)
+                    R.id.radioRectangleB -> diameterB * (widthB ?: 1.0)
+                    else -> 0.0
+                }
+
+                finalPriceB = priceB * inchesSquaredB
+                sizeB = inchesSquaredB
+                resultTextB.text = "Final Price B: \$${String.format("%.2f", finalPriceB)}"
+            } else {
+                resultTextB.text = "Please enter valid inputs."
+            }
+
+            // Set which pizza is cheaper as final choice
+            if (finalPriceA < finalPriceB) {
+                finalChoice.text = "Choose A!"
+            } else if (finalPriceB < finalPriceA) {
+                finalChoice.text = "Choose B!"
+            } else if (finalPriceA == finalPriceB) {
+                if (sizeA > sizeB) {
+                    finalChoice.text = "Choose A!"
+                } else if (sizeB > sizeA) {
+                    finalChoice.text = "Choose B!"
+                } else {
+                    finalChoice.text = "Choose either!"
+                }
+            } else {
+                finalChoice.text = "You did something wrong, figure it out.  This ain't on us chief"
+            }
+        }
+    }
+}
---
+++ pizza_price/README.md
@@ -0,0 +1,27 @@
+In-Class AI for Mobile Apps
+=======================
+
+# Teammates
+
+- Jake Hammond
+- Macaden Murrell
+- Brian Bizon
+- Vladimir Herdman
+
+## Use Notes
+
+In terms of what we have in the repo here, we just wanted to put what we
+changed explicitly.  We realize, however, that due to android IDE / Kotlin
+theme and package naming qualities, just copy pasting from one file can change
+how the app will run, even stopping it, one issue we came across being using
+slightly different project names stopping copying and pasting code from working.
+
+To combat this, we have also uploaded in the folder named PizzaPriceCalc/ the entire
+working directory for the android project from one of our members, one that had the
+app running and working.  Hopefully, that structure can be taken and ran if copy pasting
+the changed files does not work.
+
+The .gradle/, build/, .idea/, and local.properties/ folders were also gitignored from the
+total upload.
+
+## Running application
